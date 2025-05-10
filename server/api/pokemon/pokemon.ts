d7fd59da7c8764e19c26f2d3026c35cf06f5abf9
arm203 <ochiohita@gmail.com>
2025-05-10 18:36:41 +0900

    ポケモン・技データをデータベースから取得する機能を作成

---
+++ server/api/pokemon/pokemon.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { Pokemon } from "../../type/pokemon.type";
+
+const prisma = new PrismaClient();
+
+export async function pokemon_getter(pokemon_id:number):Promise<Pokemon> {
+    const data = await prisma.pokemon.findFirst({ where: {pokemon_id} });
+    if(data){
+        const pokemon:Pokemon = {
+            pokemon_id:pokemon_id,
+            name:data.name,
+            type1:data.type1,
+            type2:data.type2,
+            front_image:data.front_image,
+            back_image:data.back_image,
+            base_hp:data.base_hp,
+            base_attack:data.base_attack,
+            base_defence:data.base_defence,
+            base_special_attack:data.base_special_attack,
+            base_special_defence:data.base_special_defence,
+            base_speed:data.base_speed,
+            evolve_level:data.evolve_level,
+            move_list:data.move_list,
+        }
+        return pokemon;
+    }
+    return null;
+}
