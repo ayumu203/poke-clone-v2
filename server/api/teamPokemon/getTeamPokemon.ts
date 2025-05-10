e66178687f2f4375d548e22feeb8550f1ef1d927
arm203 <ochiohita@gmail.com>
2025-05-10 21:13:29 +0900

    fix:手持ちポケモンデータ取得部分の修正

--- server/api/teamPokemon/getTeamPokemon.ts
+++ server/api/teamPokemon/getTeamPokemon.ts
@@ -6,6 +6,14 @@ const prisma = new PrismaClient();
 export async function getTeamPokemon(player_id: string, index: number): Promise<TeamPokemon> {
     const data:TeamPokemon = await prisma.teamPokemon.findFirst({ where: { player_id, index } });
     if (!data) return data;
-    return data;
+    const teamPokemon: TeamPokemon = {
+        player_id: player_id,
+        index: index,
+        pokemon_id: data.pokemon_id,
+        level: data.level,
+        exp: data.exp,
+        move_list: data.move_list,
+    }
+    return teamPokemon;
     
 }
