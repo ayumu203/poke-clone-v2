43488e4365dbefdff961ab8fc28dcb2d7e0c35e7
arm203 <ochiohita@gmail.com>
2025-05-17 23:35:25 +0900

    update:実装予定を更新

--- server/api/teamPokemon/resisterFirstPokemon.ts
+++ server/api/teamPokemon/resisterFirstPokemon.ts
@@ -1,34 +1,36 @@
 import { PrismaClient } from "@prisma/client";
-import { isTeamPokemon } from "./isTeamPokemon";
 import { getPokemon } from "../pokemon/pokemon";
 import { getMove } from "../move/move";
 import { MAX_MOVE_COUNT } from "../../const/move_count.const";
 import { Move } from "../../type/move.type";
+import { Pokemon } from "../../type/pokemon.type";
+import { TeamPokemon } from "../../type/teamPokemon.type";
 
 const prisma = new PrismaClient();
 
-export async function registerTeamPokemon(player_id:string,pokemon_id:number,index:number):Promise<void> {
-    const exist:Boolean = await isTeamPokemon(player_id,index);
-    const move_list:number[] = [];
-    if(!exist){
-        const pokemon = await getPokemon(pokemon_id);
-        if(!pokemon){
-            throw new Error("ポケモンが見つかりません");
-        }
-        else {
-            for(let i=0;i<MAX_MOVE_COUNT;i++){
-                const move:Move = await getMove(pokemon.move_list[i]);
-                move_list.push(move!.move_id);
-            }
-        }
-        await prisma.teamPokemon.create({data: {
-            player_id:player_id,
-            index:index,
-            pokemon_id:pokemon_id,
-            level:1,
-            exp:0,
-            move_list:move_list
-        }});
+export async function registerTeamPokemon(player_id: string, pokemon_id: number, index: number): Promise<TeamPokemon> {
+    const move_list: number[] = [];
+    const pokemon:Pokemon = await getPokemon(pokemon_id);
+    if (!pokemon) {
+        throw new Error("ポケモンが見つかりません");
     }
-    return ;
+    else {
+        for (let i = 0; i < MAX_MOVE_COUNT; i++) {
+            const move: Move = await getMove(pokemon.move_list[i]);
+            move_list.push(move!.move_id);
+        }
+    }
+    await prisma.teamPokemon.create({
+        data: {
+            player_id: player_id,
+            index: index,
+            pokemon_id: pokemon_id,
+            level: 1,
+            exp: 0,
+            move_list: move_list
+        }
+    });
+
+    const teamPokemon: TeamPokemon = await prisma.teamPokemon.findFirst({ where: { player_id, index } });
+    return teamPokemon;
 }
--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'APIサーバーが稼働中です' });
 });
 
-// playerのデータを参照・データが存在しない場合は登録まで行うAPI
+// playerのデータを参照・データが存在しない場合は登録を行うAPI
 app.post('/player', async (req, res) => {
   const player_id: string = req.body.player_id;
   const exist: Boolean = await isPlayer(player_id);
@@ -31,27 +31,21 @@ app.post('/player', async (req, res) => {
     const player: Player = await getPlayer(player_id);
     res.status(200).json(player);
   }
-  else {
+  else if (!exist) {
+    // プレイヤーID(SupabaseのUID),デフォルト名トレーナー君を設定
     const player_id: string = req.body.player_id;
     const name: string = "トレーナー君";
-    await registerPlayer(player_id, name);
-    const player: Player = await getPlayer(player_id);
-    res.status(200).json({ player: player });
+    const player: Player = await registerPlayer(player_id, name);
+    res.status(200).json(player);
   }
 });
 
-// playerのデータを登録するAPI
+// playerのデータを登録するAPI(プレイヤー名の変更等で使用予定)
 app.post('/player/register', async (req, res) => {
   const player_id: string = req.body.player_id;
   const name: string = req.body.name;
-  const exist: Boolean = await isPlayer(player_id);
-  if (!exist) {
-    await registerPlayer(player_id, name);
-    res.status(200).json({ message: "登録完了" });
-  }
-  else {
-    res.status(400).json({ message: "登録に失敗" });
-  }
+  const palyer: Player = await registerPlayer(player_id, name);
+  res.status(200).json(palyer);
 });
 
 // 最初のポケモンの選択肢のデータを送信するAPI
@@ -74,19 +68,18 @@ app.post('/first-pokemon/register', async (req, res) => {
 
 
   if (!player_exist) {
-    res.status(404).json({ message: "プレイヤーが登録されていません" });
+    res.status(200).json(false);
     return;
   }
 
   if (pokemon_exist) {
-    res.status(200).json({ exist: true });
+    res.status(200).json(false);
   }
 
-  // res.status(200).json({ pokemon_exist });
   if (!pokemon_exist) {
     if (pokemon_id === 494 || pokemon_id === 495 || pokemon_id === 501) {
       await registerTeamPokemon(player_id, pokemon_id, index);
-      res.status(200).json({ exist: false });
+      res.status(200).json(true);
     }
   }
 });
@@ -96,13 +89,8 @@ app.post('/team-pokemon', async (req, res) => {
   const player_id: string = req.body.player_id;
   const index: number = req.body.index;
   const exist: Boolean = await isTeamPokemon(player_id, index);
-  if (exist) {
-    const teamPokemon = await getTeamPokemon(player_id, index);
-    res.status(200).json(teamPokemon);
-  }
-  else {
-    res.status(200).json(null);
-  }
+  const teamPokemon = await getTeamPokemon(player_id, index);
+  res.status(200).json(teamPokemon);
 });
 
 app.post('/team-pokemon/register', async (req, res) => {
@@ -112,13 +100,13 @@ app.post('/team-pokemon/register', async (req, res) => {
   const player_exist: Boolean = await isPlayer(player_id);
 
   if (!player_exist) {
-    res.status(404).json({ message: "プレイヤーが登録されていません" });
+    res.status(200).json(false);
     return;
   }
 
   if (pokemon_id >= 494 && pokemon_id <= 650) {
     await registerTeamPokemon(player_id, pokemon_id, index);
-    res.status(200).json({ exist: false });
+    res.status(200).json(false);
   }
 });
 
